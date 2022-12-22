842e6dd6c58841f1298967ef73dd5e39d6fd14a7
My_IDE_codes_java <d3kruze@gmail.com>
2022-12-23 00:32:57 +0530

    minor change in home.jsx2

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -19,7 +19,7 @@ const Home = () => {
             I'm a Full Stack Developer
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
-            I am have 1 year of experience building and designing software.
+            I have 1 year of experience building and designing software.
             Currently, I love to work on web application using technologies like
             React,Tailwind,Express JS , Node JS and MongoDB.
           </p>
